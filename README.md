9020d43ea7e76f334276822b77d37faa554ea6b8
MandarinOrange <istars0110@gmail.com>
2021-10-17 21:45:36 +0800

    二叉搜索树中第K小的元素

--- README.md
+++ README.md
@@ -16,3 +16,4 @@ leetcode solution by golang
 13. 排列硬币 leetcode441: https://leetcode-cn.com/problems/arranging-coins/
 14. 外观数列 leetcode38: https://leetcode-cn.com/problems/count-and-say/
 15. 给表达式添加运算符 leetcode282: https://leetcode-cn.com/problems/expression-add-operators/
+16. 二叉搜索树中第K小的元素 leetcode230: https://leetcode-cn.com/problems/kth-smallest-element-in-a-bst/
---
+++ src/kthSmallest/kthSmallest.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+
+/*
+16. 二叉搜索树中第K小的元素 leetcode230: https://leetcode-cn.com/problems/kth-smallest-element-in-a-bst/
+*/
+
+//Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func kthSmallest(root *TreeNode, k int) int {
+	stack := []*TreeNode{}
+	for {
+		for root != nil {
+			stack = append(stack, root)
+			root = root.Left
+		}
+		stack, root = stack[:len(stack)-1], stack[len(stack)-1]
+		k--
+		if k == 0 {
+			return root.Val
+		}
+		root = root.Right
+	}
+}
+
+//先序遍历
+var i = -1
+
+func HaveEmptyCreate(arr []int) *TreeNode {
+	i = i + 1
+	if i >= len(arr) {
+		return nil
+	}
+	var t TreeNode
+	if arr[i] != -1 {
+		t = TreeNode{arr[i], nil, nil}
+		t.Left = HaveEmptyCreate(arr)
+		t.Right = HaveEmptyCreate(arr)
+	} else {
+		return nil
+	}
+	return &t
+}
+
+func main() {
+	arr := []int{3, 1, -1, 2, 4} //-1表示nil
+	root := HaveEmptyCreate(arr)
+	k := 1
+	fmt.Println(kthSmallest(root, k))
+}
