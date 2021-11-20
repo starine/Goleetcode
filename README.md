af8e648c150cb6a8286e306fd30f092d942a468a
MandarinOrange <istars0110@gmail.com>
2021-11-20 14:56:05 +0800

    594. 最长和谐子序列

--- README.md
+++ README.md
@@ -53,3 +53,4 @@ leetcode solution by golang
 50. 最大单词长度乘积 leetcode318: https://leetcode-cn.com/problems/maximum-product-of-word-lengths/
 51. 二叉树的坡度 leetcode563: https://leetcode-cn.com/problems/binary-tree-tilt/
 52. 整数替换 leetcode397: https://leetcode-cn.com/problems/integer-replacement/
+53. 最长和谐子序列 leetcode594: https://leetcode-cn.com/problems/longest-harmonious-subsequence/
--- src/findLHS/findLHS.go
+++ src/findLHS/findLHS.go
@@ -1,1 +1,25 @@
 package main
+
+import "fmt"
+
+/*
+594. 最长和谐子序列 https://leetcode-cn.com/problems/longest-harmonious-subsequence/
+*/
+
+func findLHS(nums []int) (ans int) {
+	cnt := map[int]int{}
+	for _, num := range nums {
+		cnt[num]++
+	}
+	for num, c := range cnt {
+		if cnt[num+1] > 0 && c+cnt[num+1] > ans {
+			ans = c + cnt[num+1]
+		}
+	}
+	return
+}
+
+func main() {
+	nums := []int{1, 3, 2, 2, 5, 2, 3, 7}
+	fmt.Println(findLHS(nums))
+}
